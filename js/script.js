b934beebe03a5cc520b2c58599ef89f67c56ff31
CuriousMagpie <eilatan@gmail.com>
2021-06-02 10:59:12 -0400

    Project part 1

--- js/script.js
+++ js/script.js
@@ -6,6 +6,24 @@ const gitUser = async function() {
   const userInfo = await fetch(`https://api.github.com/users/${username}`);
   const data = await userInfo.json();
   console.log(data);
+  showUserData(data);
 };
 
 gitUser();
+
+//function to display user data
+const showUserData = function(data) {
+  const userDiv = document.createElement("div");
+  userDiv.classList.add("user-info");
+  userDiv.innerHTML = `
+    <figure>
+      <img alt="user avatar" src=${data.avatar_url} />
+    </figure>
+    <div>
+      <p><strong>Name:</strong> ${data.name}</p>
+      <p><strong>Bio:</strong> ${data.bio}</p>
+      <p><strong>Location:</strong> ${data.location}</p>
+      <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+    </div>`;
+    overview.append(userDiv);
+};
