89bc8d4b0893f34d0745d7da512c4a2251457ff3
CuriousMagpie <eilatan@gmail.com>
2021-06-02 19:00:21 -0400

    Working on part 2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 const overview = document.querySelector(".overview");
 const username = "CuriousMagpie";
+const repoList = document.querySelector(".repos");
 
-//function to call GitHub API
+//function to call GitHub API for user data
 const gitUser = async function() {
   const userInfo = await fetch(`https://api.github.com/users/${username}`);
   const data = await userInfo.json();
@@ -26,4 +27,33 @@ const showUserData = function(data) {
       <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
     </div>`;
     overview.append(userDiv);
+};
+
+//function to call GitHub API for repo data
+const gitRepos = async function() {
+  const repoInfo = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+  const repoData = await repoInfo.json();
+  console.log(repoData);
+  displayRepos(repoData);
+};
+
+gitRepos();
+
+//function to display repo data
+// const showRepoData = function(repos) {
+//   for (const repo of repos) {
+//     const eachRepo = document.createElement("li");
+//     eachRepo.classList.add("repo");
+//     eachRepo.innerHTML = `<h3>${repo.name}</h3>`;
+//     repoList.append(eachRepo);
+//   }
+// };
+
+const displayRepos = function (repos) {
+  for (const repo of repos) {
+    const repoItem = document.createElement("li");
+    repoItem.classList.add("repo");
+    repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+    repoList.append(repoItem);
+  }
 };
